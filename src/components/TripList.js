3763ab177164967532d7313a1924ddbbed836736
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-07-31 15:38:14 +0300

    move the fetch logic onto its function and use useCallback

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,15 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./TripList.css";
 export default function TripList() {
+  const [url, setUrl] = useState("http://localhost:3000/trips");
   const [trips, setTrips] = useState([]);
 
+  const fetchTrips = useCallback(async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setTrips(json);
+  }, [url]);
+
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
-  }, []);
+    fetchTrips();
+  }, [fetchTrips]);
 
   console.log(trips);
+
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
@@ -19,6 +25,17 @@ export default function TripList() {
           <p>{trip.price}</p>
         </li>
       ))}
+
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?location=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
